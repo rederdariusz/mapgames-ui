32b9b8ab4a11796c1bb60e8d9d632ba1b386f781
Dariusz Reder <rederdariusz@gmail.com>
2024-07-01 14:56:09 +0200

    Add africa + styles corrections

---
+++ src/app/africa/africa.component.ts
@@ -0,0 +1,38 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+} from '@angular/core';
+import { Map } from '../../shared/abstracts/map.class';
+
+@Component({
+  selector: 'africa',
+  standalone: true,
+  templateUrl: './africa.component.html',
+  styleUrl: './africa.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AfricaComponent extends Map implements OnInit {
+  private _svg = viewChild.required<ElementRef>('svg');
+
+  constructor() {
+    super('Africa', []);
+  }
+
+  public ngOnInit(): void {
+    this.countries = [
+      ...this._svg().nativeElement.getElementsByTagName('path'),
+    ].map((p) => p.getAttribute('data-name'));
+    console.log(this.countries)
+  }
+
+  public override guess(country: string): boolean {
+    let path = this._svg().nativeElement.querySelector(`[data-name="${country}"]`);
+    if (!path) return false;
+
+    path.style.fill = '#06c258';
+    return true;
+  }
+}
